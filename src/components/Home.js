bf5bc6df7499cd8321ec2f4a47190a2e386643dd
Dianht <dinath.sh@outlook.fr>
2022-01-07 00:57:52 +0100

    button add

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,54 @@ import React from 'react';
 import '../styles/Home.css';
 import { ReactComponent as HomeLogo } from '../assets/homeLogo.svg';
 import { useMousePosition } from './useMousePosition';
+import Stack from '@mui/material/Stack';
+import ButtonUnstyled, {
+	buttonUnstyledClasses,
+} from '@mui/base/ButtonUnstyled';
+import { styled } from '@mui/system';
+
+const myColor = {
+	500: '#FDC45C',
+	600: '#C892FF',
+	700: '#E0A9B4',
+};
+
+const CustomButtonRoot = styled('button')`
+	font-family: IBM Plex Sans, sans-serif;
+	font-weight: bold;
+	font-size: 0.875rem;
+	background-color: ${myColor[500]};
+	padding: 1vh 5vw;
+	border-radius: 30px;
+	color: white;
+	transition: all 150ms ease;
+	cursor: pointer;
+	border: none;
+	margin-top: 10px;
+
+	&:hover {
+		background-color: ${myColor[600]};
+	}
+
+	&.${buttonUnstyledClasses.active} {
+		background-color: ${myColor[700]};
+	}
+
+	&.${buttonUnstyledClasses.focusVisible} {
+		box-shadow: 0 4px 20px 0 rgba(61, 71, 82, 0.1),
+			0 0 0 5px rgba(0, 127, 255, 0.5);
+		outline: none;
+	}
+
+	&.${buttonUnstyledClasses.disabled} {
+		opacity: 0.5;
+		cursor: not-allowed;
+	}
+`;
+
+function CustomButton(props) {
+	return <ButtonUnstyled {...props} component={CustomButtonRoot} />;
+}
 
 function Stars(x0, y0) {
 	let pos = x0 + ' ' + y0 + ' 628 335';
@@ -371,6 +419,7 @@ function Home() {
 					Hi. I'm Dinath. <br /> A CS Student.
 				</h1>
 				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+				<CustomButton>Button</CustomButton>
 			</div>
 			<div className='homeRightSide'>{svg}</div>
 		</div>
