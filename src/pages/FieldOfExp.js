a32d5d8264dc11f2907daeeed516b687cc762912
DinathSh <dinath.sh@outlook.fr>
2022-01-21 01:39:24 +0100

    work css

--- src/pages/FieldOfExp.js
+++ src/pages/FieldOfExp.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import '../styles/FieldOfExp.css';
 import { PersonnalColor } from '../helpers/Color';
 
@@ -16,60 +16,62 @@ function FieldOfExp() {
 	let randomColor = PersonnalColor[randomNumber];
 
 	return (
-		<motion.div
-			className='fieldExp'
-			initial='hidden'
-			whileInView='visible'
-			viewport={{ once: true }}
-			transition={{ duration: 0.5 }}
-			variants={{
-				visible: { opacity: 1, scale: 1 },
-				hidden: { opacity: 0, scale: 0 },
-			}}>
-			<div className='fieldExpResume'>
-				<div className='fieldExpText'>
-					<h1>
-						Field of <font color={randomColor}>Experience</font>
-					</h1>
-					<p>
-						Lorem ipsum dolor sit amet consectetur, adipisicing
-						elit. Atque, velit,
-						<br /> corrupti reprehenderit doloremque ducimus iusto
-						laudantium quisquam <br />
-						earum provident quidem dolores itaque debitis,
-						<br />
-						<br /> sapiente tenetur quaerat ullam id nemo
-						asperiores. Lorem ipsum dolor sit amet consectetur
-						adipisicing elit. Sunt maiores ullam cum odit culpa
-						laudantium. Voluptas nostrum optio eveniet corrupti
-						ducimus facilis deserunt voluptatibus, fugit tenetur
-						pariatur dolore tempore rerum.
-					</p>
+		<AnimatePresence>
+			<motion.div
+				className='fieldExp'
+				initial='hidden'
+				whileInView='visible'
+				viewport={{ once: true }}
+				transition={{ duration: 0.5 }}
+				variants={{
+					visible: { opacity: 1, scale: 1 },
+					hidden: { opacity: 0, scale: 0 },
+				}}>
+				<div className='fieldExpResume'>
+					<div className='fieldExpText'>
+						<h1>
+							Field of <font color={randomColor}>Experience</font>
+						</h1>
+						<p>
+							Lorem ipsum dolor sit amet consectetur, adipisicing
+							elit. Atque, velit,
+							<br /> corrupti reprehenderit doloremque ducimus
+							iusto laudantium quisquam <br />
+							earum provident quidem dolores itaque debitis,
+							<br />
+							<br /> sapiente tenetur quaerat ullam id nemo
+							asperiores. Lorem ipsum dolor sit amet consectetur
+							adipisicing elit. Sunt maiores ullam cum odit culpa
+							laudantium. Voluptas nostrum optio eveniet corrupti
+							ducimus facilis deserunt voluptatibus, fugit tenetur
+							pariatur dolore tempore rerum.
+						</p>
+					</div>
 				</div>
-			</div>
-			<div className='fieldExpList'>
-				<div className='fieldExpLine'>
-					{SkillList1.map((item, key) => {
-						return <ExperienceItem key={key} skills={item} />;
-					})}
+				<div className='fieldExpList'>
+					<div className='fieldExpLine'>
+						{SkillList1.map((item, key) => {
+							return <ExperienceItem key={key} skills={item} />;
+						})}
+					</div>
+					<div className='fieldExpLine'>
+						{SkillList2.map((item, key) => {
+							return <ExperienceItem key={key} skills={item} />;
+						})}
+					</div>
+					<div className='fieldExpLine'>
+						{SkillList3.map((item, key) => {
+							return <ExperienceItem key={key} skills={item} />;
+						})}{' '}
+					</div>
+					<div className='fieldExpLine'>
+						{SkillList4.map((item, key) => {
+							return <ExperienceItem key={key} skills={item} />;
+						})}{' '}
+					</div>
 				</div>
-				<div className='fieldExpLine'>
-					{SkillList2.map((item, key) => {
-						return <ExperienceItem key={key} skills={item} />;
-					})}
-				</div>
-				<div className='fieldExpLine'>
-					{SkillList3.map((item, key) => {
-						return <ExperienceItem key={key} skills={item} />;
-					})}{' '}
-				</div>
-				<div className='fieldExpLine'>
-					{SkillList4.map((item, key) => {
-						return <ExperienceItem key={key} skills={item} />;
-					})}{' '}
-				</div>
-			</div>
-		</motion.div>
+			</motion.div>
+		</AnimatePresence>
 	);
 }
 
